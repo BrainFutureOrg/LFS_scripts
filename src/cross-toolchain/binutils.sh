2ca4485d2dd4f189248fb769a66cb36f43b1e495
Kosenko <alexandkosenko@gmail.com>
2024-09-22 00:20:08 +0300

    cross-toolchain scripts

---
+++ src/cross-toolchain/binutils.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -v build
+cd build
+
+time { ../configure --prefix=$LFS/tools \
+    --with-sysroot=$LFS \
+    --target=$LFS_TGT \
+    --disable-nls \
+    --enable-gprofng=no \
+    --disable-werror \
+    --enable-new-dtags \
+    --enable-default-hash-style=gnu \
+    && make && make install; }
